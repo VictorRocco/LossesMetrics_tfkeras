ffaf8897ee5acac9b27097444da5d8964d076816
Victor Rocco <victor_rocco@hotmail.com>
2022-10-22 10:01:31 -0300

    V0.2.4: updated stacked_bBCEm.

--- LossesMetrics/stacked_bBCEm.py
+++ LossesMetrics/stacked_bBCEm.py
@@ -15,22 +15,20 @@ import tensorflow.keras.backend as K
 class stacked_bBCEm(tf.keras.metrics.Metric):
 
 	def __init__(self, name="stacked_bBCEm", **kwargs):
-        			
-		super().__init__(name=name, **kwargs)
+		super(stacked_bBCEm, self).__init__(name=name, **kwargs)
 		self.metric_fnc = tf.keras.metrics.BinaryCrossentropy()
 
 	def update_state(self, y_true, y_pred, sample_weight=None):
-
-		self.y_true = y_true
-		self.y_pred = y_pred
+		y_pred_unstacked = tf.unstack(y_pred)
+		self.metric_fnc.update_state(y_true, y_pred_unstacked[0])
 
 	def result(self):
+		return self.metric_fnc.result()
 
-		y_pred_unstacked = tf.unstack(self.y_pred)
-		return self.metric_fnc(self.y_true, y_pred_unstacked[0])
+	def reset_state(self):
+		self.metric_fnc.reset_state()
 
 	def get_config(self):
-
 		config = super().get_config()
 		return config
 
